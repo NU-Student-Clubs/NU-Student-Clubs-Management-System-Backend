a37a3b8ea95fe96815bfdba7f208c0897177f8e8
Gehad Almolla <gehad.almolla@gmail.com>
2025-12-28 02:47:59 +0200

    fix/corrected homepage project declaration

--- src/main/java/com/nu/clubs/clubs_backend/controller/HomeController.java
+++ src/main/java/com/nu/clubs/clubs_backend/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.nu.clubs.clubs_bakend.controller;
+package com.nu.clubs.clubs_backend.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
